6cf5d557ab19785ff0006b43b47847a20c1f28a9
adj-dev <andrew.johnson2073@gmail.com>
2019-05-18 17:08:54 -0500

    Got Bandsintown up and running

--- liri.js
+++ liri.js
@@ -88,6 +88,31 @@ function callOmdb(title) {
     });
 }
 
+// Make a call to the Bandsintown API
+function callBandsintown(artist) {
+  axios
+    .get(`https://rest.bandsintown.com/artists/${artist}/events?app_id=secret`)
+    .then(function (response) {
+      // Show how many results were found
+      console.log(`\nFound ${response.data.length} upcoming events for ${artist}\n\n-  *  -  *  -  *  -  *  -\n`);
+
+      // Iterate through each result and display the data
+      response.data.forEach(function (event) {
+        let { name, city, region, country } = event.venue;
+        let { datetime } = event;
+
+        console.log(`Venue: ${name}\nLocation: ${city}, ${region}, ${country}\nWhen: ${datetime}`);
+        // If there are tickets available provide a link
+        if (event.offers.length !== 0) {
+          let { url } = event.offers[0];
+          console.log(`Tickets available here: ${url}`);
+        }
+        // Separator
+        console.log(`\n-  *  -  *  -  *  -  *  -\n`);
+      });
+    });
+}
+
 /*
 This is ground control, where all the magic happens. Major Tom is still out there somewhere.
 */
@@ -120,7 +145,7 @@ arg = arg.join(' ');
 
 switch (command) {
   case 'concert-this':
-    console.log('concert');
+    callBandsintown(arg);
     break;
   case 'spotify-this-song':
     callSpotify(arg, numOfResults);
