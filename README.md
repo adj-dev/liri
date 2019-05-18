ae371e016fb1a6f0abe9d2eb2d493b925e767fcf
adj-dev <andrew.johnson2073@gmail.com>
2019-05-18 14:50:31 -0500

    Added the ability to choose number of spotify results

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # liri-node-app
 Find songs from Spotify, shows from Bands In Town, and movies from OMDB
 
-Can search for something either surrounded your entire search query with double-quotes or without the quotes - it's up to you!
-
 When you search for a song the Spotify API will return you the top 5 results.
   - Want to add: specify the number of results you want
+
+### Entering arguments
+When adding any search term the user has the choice of entering an "explicit string", or an implicit string (no quotes). Liri will automatically handle it. If there are no results Liri will let the user know that 0 results were found. 
+
+### Special Features
+When using `spotify-this-song` the user has the option to specify a desired number of results by adding an additional argument: `r=[1-20]`. For example, the user could add `r=15` after writing out the song title and the application would return 15 results; `spotify-this-song "wonderwall" r=2` would return 2 songs related to "wonderwall". The user can ask for up to 20 results, and the default is 5. If the user attempts to use a number that is out of range, the default is used. Additionally, this argument can be added before or after the song title but must come after `spotify-this-song`.
--- liri.js
+++ liri.js
@@ -1,3 +1,27 @@
+/**
+ * Regular Expression IndexOf for Arrays
+ * This little addition to the Array prototype will iterate over an array
+ * and return the index of the first element which matches the provided
+ * regular expression.
+ * Note: This will not match on objects.
+ * Credit: This is not my intellectual property. http://creativenotice.com/2013/07/regular-expression-in-array-indexof/
+ */
+if (typeof Array.prototype.reIndexOf === 'undefined') {
+  /**
+   * @param {RegEx} rx The regular expression to test with. E.g. /-ba/gim
+   * @return {Numeric} -1 means not found
+   */
+  Array.prototype.reIndexOf = function (rx) {
+    for (var i in this) {
+      if (this[i].toString().match(rx)) {
+        return i;
+      }
+    }
+    return -1;
+  };
+}
+
+
 // Dependencies
 require('dotenv').config();
 const axios = require('axios');
@@ -10,30 +34,54 @@ const { spotifyCred, omdbCred, bandsintownCred } = require('./keys.js');
 const spotify = new Spotify(spotifyCred);
 
 // Define a function for fetching Spotify data
-function callSpotify(song) {
-  spotify.search({ type: 'track', query: song, limit: 5 }, function (err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    }
-    console.log('\nTop 5 songs\n\n-  *  -  *  -  *  -  *  -\n');
-    data.tracks.items.forEach(function (item) {
-      /*
-      The following console.log might look ugly, but it was the least expensive way I could think of
-      to log the artist info to the console. The last injected variable in the template literal is a 
-      tertiary expression to accomodate for the occasionally unavailable preview url: if the url is 
-      present it will be set to the value of preview_url, otherwise if the url is NOT present it will 
-      evaluate to a string that notifies the user.
-      */
-      console.log(`"${item.name}" by ${item.album.artists[0].name}\nAlbum: ${item.album.name}\nPreview: ${item.preview_url ? item.preview_url : 'preview unavailable'}\n\n-  *  -  *  -  *  -  *  -\n`)
-    });
-  });
+function callSpotify(song, num) {
+  spotify
+    .search({ type: 'track', query: song, limit: num },
+      // Handle response
+      function (err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+
+        console.log(`\nTop ${num} results\n\n-  *  -  *  -  *  -  *  -\n`);
+
+        // Inform the user if there are no results
+        if (data.tracks.items.length === 0) {
+          return console.log(`Found 0 results for "${song}"\n`);
+        }
+
+        // Iterate through the results and display info for each song
+        data.tracks.items.forEach(function (item) {
+          /*
+          The following console.log might look ugly, but it was the most straight-forward way I could think of
+          to log the artist info to the console. The last injected variable in the template literal is a 
+          tertiary expression to accomodate for the occasionally unavailable preview url: if the url is 
+          present it will be set to the value of preview_url, otherwise if the url is NOT present it will 
+          evaluate to a string that notifies the user.
+          */
+          console.log(`"${item.name}" by ${item.album.artists[0].name}\nAlbum: ${item.album.name}\nPreview: ${item.preview_url ? item.preview_url : 'preview unavailable'}\n\n-  *  -  *  -  *  -  *  -\n`)
+        });
+      });
 }
 
 // Define a function for fetching OMDB data
 function callOmdb(title) {
-  axios.get(`http://www.omdbapi.com/?apikey=${omdbCred.key}&t=${title}`)
+  axios
+    .get(`http://www.omdbapi.com/?apikey=${omdbCred.key}&t=${title}`)
     .then(function (response) {
-      console.log(response);
+      console.log('\nMovie Result(s)\n\n-  *  -  *  -  *  -  *  -\n');
+
+      // Inform the user if there are no results
+      if (response.data.Response === 'False') {
+        return console.log(`Found 0 results for "${title}"\n`);
+      }
+
+      let { Title, Year, Ratings, Country, Language, Plot, Actors } = response.data;
+      // Separately grab the imdb and rottenTomatoes ratings
+      let imdbRating = Ratings[0].Value;
+      let rtRating = Ratings[1].Value;
+
+      console.log(`${Title} (${Year}, ${Country}, ${Language})\nActors: ${Actors}\nPlot: ${Plot}\n${imdbRating} (imdb)\n${rtRating} (rottenTomatoes)\n\n-  *  -  *  -  *  -  *  -\n`);
     })
     .catch(function (err) {
       console.log(err);
@@ -44,16 +92,38 @@ function callOmdb(title) {
 This is ground control, where all the magic happens. Major Tom is still out there somewhere.
 */
 
-// Snatch the arguments declared by the user
+// Snatch the arguments given by the user
 const command = process.argv[2];
-const arg = process.argv[3];
+
+// Add all remaining args to an array, this allows the user to forgo quotes
+let arg = process.argv.slice(3);
+
+// Default number of results for spotify api
+let numOfResults = '5';
+
+// If spotify-this-song is recieved, check for user-determined number of results
+let indexOfR = arg.reIndexOf(/r=/g);
+if (command === 'spotify-this-song' && indexOfR !== -1) {
+  // If r is a number 1-20 set the value of numOfResults to value of r
+  if (arg[indexOfR].search(/^r=([1-9]|1[0-9]|20)$/g) !== -1) {
+    numOfResults = arg.splice(indexOfR, 1)[0].split('=')[1];
+    // If r is out of range then simply remove the r index from the array
+  } else {
+    arg.splice(indexOfR, 1);
+    // Inform the user that r must be set to a number between 1 and 20
+    console.log('the "r" option must be set equal to a number between 1 and 20.\nThe default of 5 results has been implemented.');
+  }
+}
+
+// API calls will expect a string as an argument
+arg = arg.join(' ');
 
 switch (command) {
   case 'concert-this':
     console.log('concert');
     break;
   case 'spotify-this-song':
-    callSpotify(arg);
+    callSpotify(arg, numOfResults);
     break;
   case 'movie-this':
     callOmdb(arg);
